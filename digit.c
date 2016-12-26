d6b9361ff4c1a244017721aad64efc2729d566d6
AshikaLakshmi <ashikalakshmi1997@gmail.com>
2016-12-26 22:14:31 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+   int n,sum=0,count=0,a;
+   printf("Enter the number :");
+   scanf("%d",&n);
+   printf("\n");
+   while(n>0)
+   {
+       a=n%10;
+       sum=sum+a;
+       n=n/10;
+       count++;
+   }
+   printf("The sum of the digits is %d",sum);
+   printf("\nThe number of digits in the number is %d",count);
+   return 0;
+}
